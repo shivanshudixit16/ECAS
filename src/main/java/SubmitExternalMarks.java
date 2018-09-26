a675e6f9495cbbfb82ffd78a70942440c4360e99
shivanshu dixit <shivanshudixit16@gmail.com>
2018-09-26 18:16:47 +0530

    v1.3

--- src/main/java/SubmitExternalMarks.java
+++ src/main/java/SubmitExternalMarks.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import database.DatabaseConnection;
+
 
 /**
  * Servlet implementation class SubmitExternalMarks
@@ -21,32 +21,32 @@ public class SubmitExternalMarks extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter out=response.getWriter();
-		Enumeration<String> e=request.getParameterNames();
-		Iterator<String> it=e.asIterator();
-		try {
-		while(it.hasNext())
-		{
-			
-				String subcode=it.next();
-				if(subcode.equals("semester")||subcode.equals("roll_no"))
-					continue;
-				Connection con =DatabaseConnection.getCon();
-				PreparedStatement ps=con.prepareStatement("update studentsub set ExternalMARKS=? where ROLL_NO=? and SUB_CODE=? and SESSION_START=? and SEMESTER=?");
-				ps.setString(1,request.getParameter(subcode));
-				ps.setString(2,request.getParameter("roll_no"));
-				ps.setString(3,subcode);
-				ps.setString(4,""+new DatabaseConnection().getCurrentSession());
-				ps.setString(5, request.getParameter("semester"));
-				ps.executeQuery();
-			    out.write("succes fuly added marks of "+subcode+"\n");
-			} 
-
-		}
-		catch (Exception ex) {
-			// TODO: handle exception
-			out.write("error\n");
-			ex.printStackTrace();
+//		PrintWriter out=response.getWriter();
+//		Enumeration<String> e=request.getParameterNames();
+//		Iterator<String> it=e.asIterator();
+//		try {
+//		while(it.hasNext())
+//		{
+//			
+//				String subcode=it.next();
+//				if(subcode.equals("semester")||subcode.equals("roll_no"))
+//					continue;
+//				Connection con =DatabaseConnection.getCon();
+//				PreparedStatement ps=con.prepareStatement("update studentsub set ExternalMARKS=? where ROLL_NO=? and SUB_CODE=? and SESSION_START=? and SEMESTER=?");
+//				ps.setString(1,request.getParameter(subcode));
+//				ps.setString(2,request.getParameter("roll_no"));
+//				ps.setString(3,subcode);
+//				ps.setString(4,""+new DatabaseConnection().getCurrentSession());
+//				ps.setString(5, request.getParameter("semester"));
+//				ps.executeQuery();
+//			    out.write("succes fuly added marks of "+subcode+"\n");
+//			} 
+//
+//		}
+//		catch (Exception ex) {
+//			// TODO: handle exception
+//			out.write("error\n");
+//			ex.printStackTrace();
 		}
 	}
 
